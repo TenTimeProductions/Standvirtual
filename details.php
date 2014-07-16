d4af20cc43f973002b542ee7c24e9d1edff95375
JoaoMalaiko <jmalaiko@gmail.com>
2014-07-16 05:07:36 +0100

    include organizaçao dos carousels
    love ya <3

--- details.php
+++ details.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
+<?php include('public/css/details.css'); ?>
 <body>
 	<div class="container">
 		<!-- Img -->
@@ -46,7 +41,7 @@
 		<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
 			
 		</div>
-		<!-- add -->
+		<!-- ad -->
 		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 			
 		</div>
@@ -56,11 +51,98 @@
 		</div>
 		<!-- related -->
 		<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
-			
+			<div class="container">
+    <div class="row">
+		<div class="col-md-12">
+                <div id="Carousel" class="carousel slide">
+                 
+                <ol class="carousel-indicators">
+                    <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#Carousel" data-slide-to="1"></li>
+                    <li data-target="#Carousel" data-slide-to="2"></li>
+                </ol>
+                 
+                <!-- Carousel items -->
+                <div class="carousel-inner">
+                    
+                <div class="item active">
+                	<div class="row">
+                	  <div class="col-md-4">
+                	  	<a href="#" class="thumbnail">
+                	  		<img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                	  	</a>
+                	  </div>
+                	  <div class="col-md-4">
+                	  	<a href="#" class="thumbnail">
+                	  		<img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                	  	</a>
+                	  </div>
+                	  <div class="col-md-4">
+                	  	<a href="#" class="thumbnail">
+                	  		<img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                	  	</a>
+                	  </div>
+                	</div><!--.row-->
+                </div><!--.item-->
+                 
+                <div class="item">
+                	<div class="row">
+                		<div class="col-md-4">
+                			<a href="#" class="thumbnail">
+                				<img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                			</a>
+                		</div>
+                		<div class="col-md-4">
+                			<a href="#" class="thumbnail">
+                				<img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                			</a>
+                		</div>
+                		<div class="col-md-4">
+                			<a href="#" class="thumbnail">
+                				<img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                			</a>
+                		</div>
+                	</div><!--.row-->
+                </div><!--.item-->
+                 
+                <div class="item">
+                	<div class="row">
+                		<div class="col-md-4">
+                			<a href="#" class="thumbnail">
+                				<img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                			</a>
+                		</div>
+                		<div class="col-md-4">
+                			<a href="#" class="thumbnail">
+                				<img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                			</a>
+                		</div>
+                		<div class="col-md-4">
+                			<a href="#" class="thumbnail">
+                				<img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
+                			</a>
+                		</div>
+                	</div><!--.row-->
+                </div><!--.item-->
+                 
+                </div><!--.carousel-inner-->
+                  <a data-slide="prev" href="#Carousel" class="left carousel-control">‹</a>
+                  <a data-slide="next" href="#Carousel" class="right carousel-control">›</a>
+                </div><!--.Carousel-->
+                 
+		</div>
+	</div>
+</div><!--.container-->
 		</div>
 		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
 			
 		</div>
 	</div>
 </body>
+<script>
+	// Stop carousel
+$('.carousel').carousel({
+  interval: false
+});
+</script>
 </html>
