abbc77eb6542f28c5d2f2d0fdfa7b5340ab40067
SrPatinhas <migascc69@gmail.com>
2014-07-19 03:32:27 +0100

    search bar melhorado

--- public/plugins/search.js
+++ public/plugins/search.js
@@ -1,38 +1,61 @@
-$(document).ready(function() {
-	$('[data-toggle=offcanvas]').click(function() {
-		$('.row-offcanvas').toggleClass('active');
+$(function () {
+	$(document).ready(function() {
+		$('[data-toggle=offcanvas]').click(function() {
+			$('.row-offcanvas').toggleClass('active');
+		});
 	});
-});
 
-$("#slider-price").ionRangeSlider({
-	min: 0,
-	max: 100000,
-	from: 1000,
-	to: 4000,
-	type: 'double',
-	step: 500,
-	prefix: "€",
-	prettify: true,
-	hasGrid: true
-});
-$("#slider-km").ionRangeSlider({
-	min: 0,
-	max: 500000,
-	from: 1000,
-	to: 4000,
-	type: 'double',
-	step: 2500,
-	postfix: " Km",
-	prettify: true,
-	hasGrid: true
-});
-$("#slider-year").ionRangeSlider({
-	min: 1970,
-	max: 2014,
-	from: 2000,
-	to: 2010,
-	type: 'double',
-	step: 1,
-	prettify: false,
-	hasGrid: true
+	$("#slider-price").ionRangeSlider({
+		min: 0,
+		max: 100000,
+		from: 1000,
+		to: 4000,
+		type: 'double',
+		step: 500,
+		prefix: "€",
+		prettify: true,
+		hasGrid: true
+	});
+	$("#slider-km").ionRangeSlider({
+		min: 0,
+		max: 500000,
+		from: 1000,
+		to: 4000,
+		type: 'double',
+		step: 2500,
+		postfix: " Km",
+		prettify: true,
+		hasGrid: true
+	});
+	$("#slider-year").ionRangeSlider({
+		min: 1970,
+		max: 2014,
+		from: 2000,
+		to: 2010,
+		type: 'double',
+		step: 1,
+		prettify: false,
+		hasGrid: true
+	});
+
+	$("#slider-CV").ionRangeSlider({
+		min: 0,
+		max: 600,
+		from: 0,
+		to: 600,
+		type: 'double',
+		step: 15,
+		prettify: false,
+		hasGrid: true
+	});
+	$("#make_select2").select2({
+		placeholder: "Selecione uma Marca",
+		allowClear:true
+	});
+	$("#model_select2").select2({
+		placeholder: "Selecione um Modelo",
+		allowClear:true,
+		enable: false
+	});
+
 });
--- search.php
+++ search.php
@@ -1,5 +1,5 @@
 <?php 
-	include('config/config.php');
+	//include('config/config.php');
 ?>
 	<div class="row row-offcanvas row-offcanvas-right">
 		<div class="sidebar-offcanvas" id="sidebar" role="navigation">
@@ -10,22 +10,31 @@
 			</p>
 			<div class="well sidebar-nav" style="max-width:300px; padding: 10px;">
 				<ul class="nav nav-list">
-					<li><label class="tree-toggler nav-header">Preço</label>
-						<ul class="nav nav-list tree">
+					<li class="divider-search"></li>
+					<li><label class="nav-header">Preço</label>
+						<ul class="nav nav-list ">
 							<li><input type="text" id="slider-price" /></li>
 						</ul>
 					</li>
-					<li><label class="tree-toggler nav-header">Km</label>
-						<ul class="nav nav-list tree">
+					<li class="divider-search"></li>
+					<li><label class="nav-header">Km</label>
+						<ul class="nav nav-list">
 							<li><input type="text" id="slider-km" /></li>
 						</ul>
 					</li>
-					<li><label class="tree-toggler nav-header">Ano</label>
-						<ul class="nav nav-list tree">
+					<li class="divider-search"></li>
+					<li><label class="nav-header">Ano</label>
+						<ul class="nav nav-list">
 							<li><input type="text" id="slider-year" /></li>
 						</ul>
 					</li>
-					<li class="divider"></li>
+					<li class="divider-search"></li>
+					<li><label class="nav-header">Potência</label>
+						<ul class="nav nav-list">
+							<li><input type="text" id="slider-CV" /></li>
+						</ul>
+					</li>
+					<li class="divider-search"></li>
 				</ul>
 
 				<div class="panel-group" id="accordion">
@@ -51,13 +60,13 @@
 					    </div>
 						<div id="collapseModelo" class="panel-collapse collapse">
 							<div class="panel-body search-bar-panel-body">
-								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
-								</div>
+								<select id="model_select2">
+									<option value="">911 </option>
+									<option value="">Cayenne </option>
+									<option value="">Boxster </option>
+									<option value="">Panamera </option>
+									<option value="">mais </option>
+								</select>
 							</div>
 					    </div>
 					</div>
@@ -70,11 +79,13 @@
 				        <div id="collapseSeccao" class="panel-collapse collapse">
 				            <div class="panel-body search-bar-panel-body">
 								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Utilitários/Sedan		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Carrinha/Combi			<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Desportivo/Coupé		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Cabrios/Roadster		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">SUV/Monovolume			<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Citadinos				<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Todo-o-terreno/Pick-up	<span class="badge">14</span></a>
 								</div>
 				            </div>
 				        </div>
@@ -88,11 +99,12 @@
 					    <div id="collapseCombustivel" class="panel-collapse collapse">
 					        <div class="panel-body search-bar-panel-body">
 								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Gasolina		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Diesel			<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">GPL				<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Híbrido			<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Eléctrico		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Qualquer Tipo	<span class="badge">14</span></a>
 								</div>
 					        </div>
 					    </div>
@@ -106,33 +118,14 @@
 				        <div id="collapseCaixa" class="panel-collapse collapse">
 				            <div class="panel-body search-bar-panel-body">
 								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Caixa Manual			<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Caixa Semi-Automática	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Caixa Automática		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Qualquer Tipo			<span class="badge">14</span></a>
 								</div>
-				            </div>
-				        </div>
+							</div>
+						</div>
 				    </div>
-					<div class="panel panel-default">
-					    <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" href="#collapsePotencia">
-					        <h4 class="panel-title">
-					            	<i class="fa fa-arrow-right search-bar-glyphicon"></i>Potência
-					        </h4>
-					    </div>
-					    <div id="collapsePotencia" class="panel-collapse collapse">
-					        <div class="panel-body search-bar-panel-body">
-								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
-								</div>
-					        </div>
-					    </div>
-					</div>
 					<div class="panel panel-default">
 					    <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" href="#collapsePintura">
 					        <h4 class="panel-title">
@@ -142,11 +135,16 @@
 					    <div id="collapsePintura" class="panel-collapse collapse">
 					        <div class="panel-body search-bar-panel-body">
 								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Preto 		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Cinzento 	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Azul 		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Branco 		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Prata 		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Outra 		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Verde 		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Vermelho 	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Laranja 	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Bege 		<span class="badge">14</span></a>
 								</div>
 					        </div>
 					    </div>
@@ -160,11 +158,11 @@
 					    <div id="collapseLotacao" class="panel-collapse collapse">
 					        <div class="panel-body search-bar-panel-body">
 								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">2 	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">3 	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">4-5 <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">6-7 <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">8-9 <span class="badge">14</span></a>
 								</div>
 					        </div>
 					    </div>
@@ -178,11 +176,9 @@
 					    <div id="collapsePortas" class="panel-collapse collapse">
 					        <div class="panel-body search-bar-panel-body">
 								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">1-3 		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">4-5 		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">6 ou mais 	<span class="badge">14</span></a>
 								</div>
 					        </div>
 					    </div>
@@ -196,11 +192,10 @@
 					    <div id="collapseArCondicionado" class="panel-collapse collapse">
 					        <div class="panel-body search-bar-panel-body">
 								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">AC					<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">AC Automático		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">AC Independente		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">AC p/ Zona Traseira	<span class="badge">14</span></a>
 								</div>
 					        </div>
 					    </div>
@@ -214,11 +209,11 @@
 					    <div id="collapseInserido" class="panel-collapse collapse">
 					        <div class="panel-body search-bar-panel-body">
 								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Ontem		<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">há 2 dias	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">há 3 dias	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">há 1 semana	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">há 1 mês	<span class="badge">14</span></a>
 								</div>
 					        </div>
 					    </div>
@@ -232,11 +227,8 @@
 					    <div id="collapseAnunciantes" class="panel-collapse collapse">
 					        <div class="panel-body search-bar-panel-body">
 								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Particulares	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Profissionais	<span class="badge">14</span></a>
 								</div>
 					        </div>
 					    </div>
@@ -250,11 +242,25 @@
 					    <div id="collapseFarois" class="panel-collapse collapse">
 					        <div class="panel-body search-bar-panel-body">
 								<div class="list-group" style="margin-bottom:0px;">
-									<a href="#" class="list-group-item">911 <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Cayenne <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Boxster <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">Panamera <span class="badge">14</span></a>
-									<a href="#" class="list-group-item">mais <span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Faróis Xenón	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Faróis Bixenón	<span class="badge">14</span></a>
+								</div>
+					        </div>
+					    </div>
+					</div>
+					<div class="panel panel-default">
+					    <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" href="#collapseTecto">
+					        <h4 class="panel-title">
+					            	<i class="fa fa-arrow-right search-bar-glyphicon"></i>Tecto
+					        </h4>
+					    </div>
+					    <div id="collapseTecto" class="panel-collapse collapse">
+					        <div class="panel-body search-bar-panel-body">
+								<div class="list-group" style="margin-bottom:0px;">
+									<a href="#" class="list-group-item">Tecto de Abrir Eléctrico	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Tecto de Abrir Panorâmico	<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Tecto Panorâmico			<span class="badge">14</span></a>
+									<a href="#" class="list-group-item">Tecto de Abrir Manual		<span class="badge">14</span></a>
 								</div>
 					        </div>
 					    </div>
@@ -269,3 +275,4 @@
 		</p>
 	</div>
 	<!--/row-->
+
